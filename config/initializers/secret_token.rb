272f53c69f25a2c6e46b02822bdba90baa60167e
Mitchell Armstrong <mitchellarmstrong@gmail.com>
2013-09-09 15:02:16 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest::Application.config.secret_token = 'ca47ae1cad538474c0e215d6820794e860274ca4311a4a8e4b26ec965913e044ba717bbcc082dc3750bca385b98444cedb560426bdc704f7e7f5f226a5190c4b'
